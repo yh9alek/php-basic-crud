a430048f83cf0649f407a253ac6f31886695e992
Yohan Alek <yh9dev@gmail.com>
2023-11-09 13:39:38 -0700

    Proyect Start

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP CRUD Básico</title>
+        <link rel="stylesheet" href="/assets/css/index.css">
+    </head>
+    <body>
+        <header>
+            <h1>PHP MYSQL CRUD</h1>
+        </header>
+    </body>
+</html>
