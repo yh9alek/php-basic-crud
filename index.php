abbe9606e3b728f528045e81800a2793f03f95da
Yohan Alek <yh9dev@gmail.com>
2023-11-09 13:54:35 -0700

    The header is finished

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP CRUD Básico</title>
-        <link rel="stylesheet" href="/assets/css/index.css">
+        <link rel="stylesheet" href="../php crud/assets/css/index.css">
     </head>
     <body>
         <header>
-            <h1>PHP MYSQL CRUD</h1>
+            <div class="container">
+                <h1>PHP MYSQL CRUD</h1>
+            </div>
         </header>
     </body>
 </html>
